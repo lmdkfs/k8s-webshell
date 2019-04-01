de6a40316d9b95ce8d16779a12864353746c2cb1
richie <zhuruiqing@finupgroup.com>
2019-04-01 16:55:46 +0800

    Add auth

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM alpine:latest
 ADD bin/k8s-webshell /data/k8s-webshell
+ADD configs/inside_finup.crt /data/inside_finup.crt
+ADD configs/inside_finup.key /data/inside_finup.key
 
 RUN chmod 755 /data/k8s-webshell
 
---
+++ pkg/api/auth.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"k8s-webshell/pkg/e"
+	"k8s-webshell/pkg/setting"
+	"k8s-webshell/pkg/utils"
+	"net/http"
+)
+
+type auth struct {
+	Username string
+	Password string
+}
+
+
+func GetAuth(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
+
+	data := make(map[string]interface{})
+	code := e.INVALID_PARAMS
+
+	if username == setting.UserName && password == setting.PassWord {
+		token, err := utils.GenerateToken(username, password)
+		if err != nil {
+			code = e.ERROR_AUTH_TOKEN
+		} else {
+			data["token"] = token
+			code = e.SUCCESS
+		}
+	} else {
+		utils.Logger.Info("username or password invalid")
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": data,
+	})
+}
