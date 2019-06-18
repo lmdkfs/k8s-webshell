102781f9dc80707215817224aa41e4afc5ed49d6
richie <zhuruiqing@finupgroup.com>
2019-06-18 22:24:09 +0800

    add metrics

--- routers/router.go
+++ routers/router.go
@@ -3,12 +3,13 @@ package routers
 import (
 	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
 	"k8s-webshell/middleware/jwt"
 	"k8s-webshell/pkg/api"
 	"k8s-webshell/pkg/setting"
 	"k8s-webshell/pkg/utils"
-
-	"github.com/gin-gonic/gin"
 )
 
 func InitRouter() *gin.Engine {
@@ -19,6 +20,7 @@ func InitRouter() *gin.Engine {
 	route.Use(utils.GinRus(utils.Logger, time.RFC3339, false))
 	route.Use(gin.Recovery())
 	route.POST("/auth", api.GetAuth)
+	route.GET("/metrics", gin.WrapH(promhttp.Handler()))
 	apiV1 := route.Group("/api")
 	apiV1.Use(jwt.JWT())
 	{
